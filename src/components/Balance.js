fe3ce6e6bb3508d55d005844af7b1b4d78179c17
Kiran Kulkarni <34923686+KiranVK98@users.noreply.github.com>
2023-04-11 01:57:17 -0500

    balance-change

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -6,7 +6,7 @@ const Balance = () => {
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div>
-      <h4>Your Balance</h4>
+      <h4>Your Balancee</h4>
       <h1>${total}</h1>
     </div>
   )
